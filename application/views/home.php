665609d9b92d92a5a4fe7bdac7f4226e1feb60f1
Dinda <amalyahrizka@gmail.com>
2025-02-26 22:27:43 +0800

    dija first push

--- application/views/home.php
+++ application/views/home.php
@@ -9,7 +9,7 @@
 <body>
     <div class="container">
         <h1 class="text-center">SELAMAT DATANG</h1>
-        <h3>Data Masyarakat</h3>
+        <h3>Data Masyarakat Moncongloe</h3>
 
         <a href="<?php echo base_url('index.php/Welcome/formInput') ?>" class="btn btn-primary">Tambah Data</a>
         

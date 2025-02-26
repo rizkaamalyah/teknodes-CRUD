6ad0f3774b2a4011d23a052472bd753b8f2884bc
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-26 22:40:40 +0800

    membuat base url untuk wawan

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/teknodesa/';
+$config['base_url'] = 'http://localhost/teknodes-CRUD/';
+// $config['base_url'] = 'http://localhost/teknodesa-dinda/teknodes-CRUD/';
 
 /*
 |--------------------------------------------------------------------------

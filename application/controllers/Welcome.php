2e2b7f5e4336b9c7856f1a9cd7990723fb4b59a4
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2025-02-26 22:42:12 +0800

    menambah method aksiDelete, AksiEdit, dan perbaiki FormEdit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -11,6 +11,8 @@ class Welcome extends CI_Controller {
 	
 	public function index()
 	{
+		// Memuat model
+		$this->load->model('Model_data');
 		$record = $this->Model_data->getData();
 		$data = array('data' => $record);
 		$this->load->view('home', $data);
@@ -23,7 +25,12 @@ class Welcome extends CI_Controller {
 
 	public function formEdit($id)
 	{
+		// Memuat model
+		$this->load->model('Model_data');
+		
+		// menjalankan kode untuk method getdatabyid
 		$record = $this->Model_data->getDataDetail($id);
+
 		$DATA = array('data' => $record);
 		$this->load->view('form_edit', $DATA);
 	}
@@ -43,4 +50,37 @@ class Welcome extends CI_Controller {
 		$this->Model_data->insertData($dataInsert);
 		redirect(base_url('index.php/Welcome'));
 	}
+
+	public function AksiEdit() {
+        // Memuat model
+        $this->load->model('Model_data');
+
+        // Mengambil data dari form
+        $nik = $this->input->post('nik');
+        $nama = $this->input->post('nama');
+        $alamat = $this->input->post('alamat');
+
+        // Data yang akan diupdate
+        $data = array(
+            'nama' => $nama,
+            'alamat' => $alamat
+        );
+
+        // Melakukan update data
+        $this->Model_data->updateData($nik, $data);
+
+        // Redirect ke halaman daftar data
+        redirect('Welcome/index');
+    }
+
+	public function aksiDelete($nik) {
+        // Memuat model
+        $this->load->model('Model_data');
+
+        // Memanggil method deleteData di model
+        $this->Model_data->deleteData($nik);
+
+        // Redirect ke halaman daftar data
+        redirect('Welcome/index');
+    }
 }
